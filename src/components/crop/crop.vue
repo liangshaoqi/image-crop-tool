14bb871b272f2f52900ce67ce0a0e730b8a56c59
梁绍奇 <918872153@qq.com>
2020-07-02 17:20:50 +0800

    [update] 边界修复以及裁剪预览

--- src/components/crop/crop.vue
+++ src/components/crop/crop.vue
@@ -1,7 +1,8 @@
 <template>
-  <div class="crop-view">
-    <img class="original-image" src="../../assets/crop.png" alt="图片" />
-    <img class="crop-original-image" src="../../assets/crop.png" alt />
+  <div class="crop-view" id='crop-view'>
+    <div class="crop-cover"></div>
+    <img @load="imageOnload($event)" id="original-image" class="original-image" src="../../assets/timg.jpg" alt="图片" />
+    <!-- <img class="crop-original-image" src="../../assets/crop.png" alt /> -->
     <!-- 裁剪框 -->
     <div id="crop-clip_view" ref="crop-clip_view" v-drag>
       <div class="square left-up" v-scale="'left-up'"></div>
@@ -13,6 +14,10 @@
       <div class="square down" v-scale="'down'"></div>
       <div class="square right-down" v-scale="'right-down'"></div>
     </div>
+    <!-- 预览 -->
+    <div class="preview-view">
+      <canvas id='clip_anvas' width="600" height="300">您的浏览器不支持canvas标签</canvas>
+    </div>
   </div>
 </template>
 
@@ -21,19 +26,50 @@
 export default {
   directives: {
     drag: function (el, params, vnode) {
-      let getPosition = vnode.context.getPositionLocation
+      let clipRangeData = vnode.context.clipRangeData
+      let createPreview = vnode.context.createPreview
       let dragEl = el
       dragEl.onmousedown = e => {
         let relativeX = e.clientX - dragEl.offsetLeft
         let relativeY = e.clientY - dragEl.offsetTop
-        let boxEl = document.getElementById('crop-clip_view')
-        console.log(getPosition(boxEl))
-        let position = getPosition(boxEl)
+        let clipEl = document.getElementById('crop-clip_view') // 裁剪框
+        let limitEl = document.getElementById('original-image') // 限制框
         document.onmousemove = e => {
           let left = e.clientX - relativeX
           let top = e.clientY - relativeY
+          // 外层信息
+          let limitLeft = limitEl.offsetLeft
+          let limitTop = limitEl.offsetTop
+          let limitWidth = limitEl.offsetWidth
+          let limitHeight = limitEl.offsetHeight
+          // 裁剪框信息
+          let clipWidth = clipEl.offsetWidth
+          let clipHeight = clipEl.offsetHeight
+          let clipLeft = clipEl.offsetLeft
+          let clipTop = clipEl.offsetTop
+          // 判断是否越界
+          if (left < 0) {
+            left = 0
+          }
+          if (top < 0) {
+            top = 0
+          }
+          
+          if (clipLeft + clipWidth >= limitWidth && left > limitWidth - clipWidth) {
+            left = limitWidth - clipWidth
+          }
+          if (clipTop + clipHeight >= limitHeight && top > limitHeight - clipHeight) {
+            top = limitHeight - clipHeight
+          }
           dragEl.style.left = left + 'px'
           dragEl.style.top = top + 'px'
+          let positionData = {
+            left,
+            top,
+            width: clipWidth,
+            height: clipHeight
+          }
+          createPreview(positionData)
         }
         document.onmouseup = () => {
           document.onmousemove = null
@@ -42,9 +78,9 @@ export default {
       }
     },
     scale: function (el, params, vNode) {
-      // console.log(vNode)
       let scaleEl = el
       let direction = params.value
+      let { createPreview, clearImage } = vNode.context
       // 获取元素相遇屏幕左边的位置
       const getPositionLocation = (node) => {
         let left = node.offsetLeft
@@ -65,18 +101,24 @@ export default {
           height
         }
       }
-
       scaleEl.onmousedown = e => {
         e.stopPropagation()
         let boxEl = document.getElementById('crop-clip_view')
-        let postion = getPositionLocation(boxEl)
-        console.log(postion)
+        let limitEl = document.getElementById('original-image')
+        // 裁剪框信息
         let offsetLeft = boxEl.offsetLeft
         let offsetTop = boxEl.offsetTop
         let offsetWidth = boxEl.offsetWidth // old裁剪框宽
         let offsetHeight = boxEl.offsetHeight
         let oldWidth = offsetWidth - 2 // 减2为border宽
         let oldHeight = offsetHeight - 2
+
+        //  限制框信息
+        let limitLeft = limitEl.offsetLeft
+        let limitTop = limitEl.offsetTop
+        let limitWidth = limitEl.width
+        let limitHeight = limitEl.height
+
         let x1 = e.clientX // 当前点击的伸缩点的x坐标
         let y1 = e.clientY
         // 左移
@@ -85,13 +127,22 @@ export default {
           let x2 = e.clientX
           let width = x1 - x2 + oldWidth
           let left = offsetLeft - (x1 - x2)
-          boxEl.style.width = width + 'px',
-            boxEl.style.left = left + 'px'
+          if (x2 - x1 > oldWidth) return
+          if (left < 0) {
+            left = 0
+            width = oldWidth + offsetLeft
+          }
+          boxEl.style.width = width + 'px'
+          boxEl.style.left = left + 'px'
         }
         const moveRight = (e) => {
           // 修改宽,定位不变
           let x2 = e.clientX
           let width = x2 - x1 + oldWidth
+          // 边界
+          if (offsetLeft + width >= limitLeft + limitWidth - 2) {
+            width = limitLeft + limitWidth - offsetLeft - 2
+          }
           boxEl.style.width = width + 'px'
         }
         const moveUp = (e) => {
@@ -99,12 +150,21 @@ export default {
           let y2 = e.clientY
           let top = offsetTop - (y1 - y2)
           let height = y1 - y2 + oldHeight
+          if (y2 - y1 > oldHeight) return
+          if (top < 0) {
+            top = 0
+            height = oldHeight + offsetTop
+          }
+          
           boxEl.style.height = height + 'px'
           boxEl.style.top = top + 'px'
         }
         const moveDown = (e) => {
           let y2 = e.clientY
           let height = y2 - y1 + oldHeight
+          if (offsetTop + height >= limitTop + limitHeight) {
+            height = limitTop + limitHeight - offsetTop
+          }
           boxEl.style.height = height + 'px'
         }
         document.onmousemove = e => {
@@ -140,6 +200,13 @@ export default {
             default:
               break
           }
+          clearImage()
+          createPreview({
+            width: boxEl.offsetWidth,
+            height: boxEl.offsetHeight,
+            left: boxEl.offsetLeft,
+            top: boxEl.offsetTop
+          })
         }
         document.onmouseup = () => {
           document.onmousemove = null
@@ -154,6 +221,29 @@ export default {
     }
   },
   methods: {
+    imageOnload(e) {
+      this.createPreview()
+    },
+    clearImage() {
+      let canvas = document.getElementById('clip_anvas')
+      let ctx = canvas.getContext('2d')
+      ctx.clearRect(0, 0, 600, 300)
+    },
+    createPreview(data) {
+      let canvas = document.getElementById('clip_anvas')
+      let ctx = canvas.getContext('2d')
+      let image = document.getElementById('original-image')
+      // 图片原始信息
+      let naturalWidth = image.naturalWidth
+      let naturalHeight = image.naturalHeight
+      let ratioX = naturalWidth / image.width
+      let ratioY = naturalHeight / image.height
+      if (data) {
+        ctx.drawImage(image, data.left * ratioX, data.top * ratioY, data.width * ratioX, data.height * ratioY, 0, 0, data.width, data.height)
+      } else {
+        ctx.drawImage(image, 0, 0, 200 * ratioX, 200 * ratioY, 0, 0, 200, 200)
+      }
+    },
     getPositionLocation(node) {
       let left = node.offsetLeft
       let top = node.offsetTop
@@ -172,6 +262,38 @@ export default {
         width,
         height
       }
+    },
+    /**
+     * 裁剪框移动和缩小放大的区域限制
+     * @params { Object } clipNode 裁剪框节点对象
+     * @params { Object } limitNode 限制范围的节点对象
+     * @returns { Object } 范围定位
+    **/
+    clipRangeData(clipNode, limitNode) {
+      // 限制裁剪框移动的节点信息
+      let limitNodeWidth = limitNode.offsetWidth
+      let limitNodeHeight = limitNode.offsetHeight
+      let limitNodeLeft = limitNode.offsetLeft
+      let limitNodeTop = limitNode.offsetTop
+      // 裁剪框节点信息
+      let clipNodeWidth = clipNode.offsetWidth
+      let clipNodeHeight = clipNode.offsetHeight
+      let clipNodeLeft = clipNode.offsetLeft
+      let clipNodeTop = clipNode.offsetTop
+      // 最终返回的范围
+      let range = {}
+      // 移动过程区域
+      if (clipNodeLeft <= 0) {
+        range.left = 0
+      } else {
+        range.left = clipNodeLeft
+      }
+      if (clipNodeTop < 0) {
+        range.top = 0
+      } else {
+        range.top = clipNodeTop
+      }
+      return range
     }
   }
 }
@@ -183,31 +305,43 @@ export default {
   height: 600px;
   background: #ccc;
   .original-image {
-    opacity: 0.5;
+    // opacity: 0.5;
     width: 600px;
-    height: auto;
+    height: 300px;
     position: absolute;
     top: 0;
     left: 0;
     user-select: none;
+    z-index: 1;
+  }
+  .crop-cover {
+    width: 600px;
+    height: 300px;
+    position: absolute;
+    top: 0;
+    left: 0;
+    z-index: 10;
   }
   .crop-original-image {
     user-select: none;
     width: 600px;
-    height: auto;
+    height: 300px;
     position: absolute;
     top: 0;
     left: 0;
-    clip: rect(0, 300px, 300px, 0);
+    clip: rect(0, 200px, 200px, 0);
+  }
+  .preview-view {
+    position: absolute;
+    top: 50%;
   }
   #crop-clip_view {
-    width: 300px;
-    height: 300px;
+    width: 200px;
+    height: 200px;
     border: 1px solid white;
     cursor: move;
     position: absolute;
-    z-index: 10;
-    // top: 10px;
+    z-index: 99;
     .square {
       width: 8px;
       height: 8px;
